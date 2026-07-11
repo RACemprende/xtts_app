eac8ec7bf509b31ecf62f22b0d2acf4809f6194e
RACemprende <racspotifycreators@gmail.com>
2026-07-11 23:22:30 +0200

    Update app.py to check streamlit status

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
-import os
-os.environ["COQUI_TOS_AGREED"] = "1"
-
 import streamlit as st
-import torch
-import transformers.pytorch_utils as ptu
 
-if not hasattr(ptu, "isin_mps_friendly"):
-    def isin_mps_friendly(elements, test_elements):
-        return torch.isin(elements, test_elements)
-    ptu.isin_mps_friendly = isin_mps_friendly
+st.title("Prueba básica")
 
-from TTS.api import TTS
-
-st.write("Import OK")
-
-@st.cache_resource
-def load_model():
-    return TTS(
-        "tts_models/es/css10/vits",
-        gpu=False
-    )
-
-st.write("Antes de cargar XTTS")
-
-tts = load_model()
-
-st.success("XTTS cargado")
-st.write(len(tts.speakers))
+st.success("La app está viva")
