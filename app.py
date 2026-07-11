578cc64014aff5c4eb68366b3c45c29933d8dd15
RACemprende <racspotifycreators@gmail.com>
2026-07-12 01:18:43 +0200

    Update app.py adding cache resource to xtts loading

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-os.environ["COQUI_TOS_AGREED"] = "1"
-
 import streamlit as st
 import torch
 import transformers.pytorch_utils as ptu
+
+os.environ["COQUI_TOS_AGREED"] = "1"
 
 if not hasattr(ptu, "isin_mps_friendly"):
     def isin_mps_friendly(elements, test_elements):
@@ -17,13 +17,22 @@ from TTS.api import TTS
 
 st.write("DESPUÉS DEL IMPORT")
 
+
+@st.cache_resource
+def load_model():
+    return TTS(
+        "tts_models/multilingual/multi-dataset/xtts_v2",
+        gpu=False
+    )
+
+
 st.write("ANTES DE CREAR TTS")
 
-tts = TTS(
-    "tts_models/es/css10/vits",
-    gpu=False
-)
+tts = load_model()
 
 st.write("DESPUÉS DE CREAR TTS")
 
 st.success("XTTS cargado")
+
+if hasattr(tts, "speakers"):
+    st.write(len(tts.speakers))
