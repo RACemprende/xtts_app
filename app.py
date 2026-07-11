a84e1f8fe54ac955422f01bfbd5cc135b5365665
RACemprende <racspotifycreators@gmail.com>
2026-07-11 22:40:42 +0200

    Update app.py to check dependecies version used by streamlit

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 import streamlit as st
-import sys
+import torch
 
+st.write("Python:")
+import sys
 st.write(sys.version)
 
-import torch
-import transformers.pytorch_utils as ptu
-
-if not hasattr(ptu, "isin_mps_friendly"):
-
-    def isin_mps_friendly(elements, test_elements):
-        return torch.isin(elements, test_elements)
-
-    ptu.isin_mps_friendly = isin_mps_friendly
-
-from TTS.api import TTS
-
-st.success("Coqui cargado correctamente")
+st.write("Torch:")
+st.write(torch.__version__)
