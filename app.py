94e06a17b91b053e6ce43abaccc10fcf30da6b92
RACemprende <racspotifycreators@gmail.com>
2026-07-11 22:16:09 +0200

    Update app.py fixing bug adding isin_mps_friendly

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+import torch
+import transformers.pytorch_utils as ptu
+
+if not hasattr(ptu, "isin_mps_friendly"):
+
+    def isin_mps_friendly(elements, test_elements):
+        return torch.isin(elements, test_elements)
+
+    ptu.isin_mps_friendly = isin_mps_friendly
+
 from TTS.api import TTS
 
 st.success("Coqui cargado correctamente")
