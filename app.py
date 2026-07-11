33435ebf365d1746ed69cd3c3efa96c31d82e719
RACemprende <racspotifycreators@gmail.com>
2026-07-11 22:57:30 +0200

    Update app.py checking XTTS loading

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-import torch
 import streamlit as st
+import torch
+import transformers.pytorch_utils as ptu
 
-st.write(torch.__version__)
+if not hasattr(ptu, "isin_mps_friendly"):
+    def isin_mps_friendly(elements, test_elements):
+        return torch.isin(elements, test_elements)
+    ptu.isin_mps_friendly = isin_mps_friendly
+
+from TTS.api import TTS
+
+st.write("Import OK")
 
 try:
-    import TTS
-    st.success("TTS importado")
+    tts = TTS(
+        "tts_models/multilingual/multi-dataset/xtts_v2",
+        gpu=False
+    )
+
+    st.success("XTTS cargado")
+    st.write(len(tts.speakers))
+
 except Exception as e:
     st.error(str(e))
