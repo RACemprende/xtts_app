619f61c898140abab1ace47ccb8566c233ef0e8c
RACemprende <racspotifycreators@gmail.com>
2026-07-11 22:44:05 +0200

    Update app.py to check torch installation

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import sys
 import torch
 
-st.write("Python:")
-import sys
-st.write(sys.version)
+st.write("Python:", sys.version)
+st.write("Torch:", torch.__version__)
 
-st.write("Torch:")
-st.write(torch.__version__)
+try:
+    import torchcodec
+    st.success("torchcodec instalado")
+except Exception as e:
+    st.error(f"torchcodec NO instalado: {e}")
